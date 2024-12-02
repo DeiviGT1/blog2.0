632f4eeb98db2c7f3054d889821c63a8cf8310d5
David <76451517+DeiviGT1@users.noreply.github.com>
2024-12-02 14:22:51 -0500

    agregamos motivai

---
+++ app/static/js/video.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const videoOverlay = document.getElementById("video-overlay");
+  const videoPlayer = document.getElementById("video-player");
+  const closeVideo = document.getElementById("close-video");
+
+  document
+    .querySelector(".project-link[data-project='5']")
+    .addEventListener("click", (event) => {
+      event.preventDefault();
+      const videoUrl = event.target.closest(".project-link").dataset.video;
+      videoPlayer.src = videoUrl; // Asigna la URL del video
+      videoOverlay.classList.remove("hidden");
+    });
+
+  closeVideo.addEventListener("click", () => {
+    videoOverlay.classList.add("hidden");
+    videoPlayer.pause(); // Pausa el video
+    videoPlayer.src = ""; // Limpia el src del video
+  });
+});
